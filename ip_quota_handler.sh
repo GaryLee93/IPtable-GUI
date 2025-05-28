6b9f3f3ad26cc0b000a139a406d4eeb00f265ad5
Ruei-Jia Huang <rueijiahuang@gmail.com>
2025-05-28 15:09:29 +0800

    fix: change html to embed local gif file instead of YT link. ip_quota_handler now properly redirects traffic to locohost:8000

--- ip_quota_handler.sh
+++ ip_quota_handler.sh
@@ -6,7 +6,7 @@ journalctl -kf | while read line; do
   IP=$(echo "$line" | grep -oP 'SRC=\K[\d.]+')
   if echo "$line" | grep -q "QUOTA_EXCEEDED"; then
     echo "QUOTA EXCEEDED!!!!!!!!!!!!!!!!!!!!!!!!!! $IP"
-    # iptables -t nat -I OUTPUT -p tcp --dport 443 -j REDIRECT --to-port 8000
+    iptables -t nat -I OUTPUT -p tcp --dport 443 -j REDIRECT --to-destination 127.0.0.1 --to-port 8000
 
     # Or: Run a custom script, send a signal, log to DB, etc.
   fi
